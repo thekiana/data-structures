1bc0d3ce508dce0601eb7ffceef802da87d8bf8d
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-10-23 15:15:05 -0700

    Make progress on functional stack implementation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,33 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
+
+  // add string to top of stack
 
   someInstance.pop = function() {
+    delete storage[count];
+    count--;
+    return storage[count];
   };
 
+  // remove and return string on top of stack
+
   someInstance.size = function() {
+    if (count < 0) {
+      return 0;
+    }
+
+    return Object.keys(storage).length;
   };
+
+  // return number of items on stack
 
   return someInstance;
 };
