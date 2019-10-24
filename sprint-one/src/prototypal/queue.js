714eed2c7ac830ffe784af31998375c1b2f2cf11
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-10-24 09:11:41 -0700

    Implement functionality for prototypal pattern of stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+  storage.count = 0;
+  storage.head = 0;
+
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+queueMethods.dequeue = function() {
+  var front = this[this.head];
+  delete this[this.head];
+  this.head++;
+  return front;
+};
+
+queueMethods.size = function() {
+  if (this.head > this.count) {
+    return 0;
+  } else if (this.count > this.head) {
+    return this.count - this.head;
+  }
+
+  return this.count;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = Object.create(stackMethods);
+  storage.count = 0;
+
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  var top = this[this.count - 1];
+  delete this[this.count];
+  this.count--;
+  return top;
+};
+
+stackMethods.size = function() {
+  if (this.count < 0) {
+    return 0;
+  }
+
+  return this.count;
+};
