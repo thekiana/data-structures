a41234d0881f948b4fb7142bb17a33135d9d8bb7
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-10-25 16:17:01 -0700

    Implement recursive functionality for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,30 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // this.value = value;
+  // Tree(this.children.push(this));
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var hasNode = this.value === target;
+
+  this.children.forEach(function(child) {
+    if (!hasNode) {
+      hasNode = child.contains(target);
+    }
+  });
+
+  return hasNode;
 };
 
 
@@ -21,4 +33,3 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-//O(log n) logarithmic
