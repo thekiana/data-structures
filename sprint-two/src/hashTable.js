219378717edd95fa9daf7081a79fc2e0fb4ba94e
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-10-26 11:14:45 -0700

    Implement hashTable functionalities without handling collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,23 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // console.log('val ', v);
+  // console.log('-----');
+  // console.log('key ', k);
+  // console.log('-----');
+  // console.log('-----');
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var toDelete = this._storage.get(index);
+  this._storage.set(index, toDelete = undefined);
 };
 
 
