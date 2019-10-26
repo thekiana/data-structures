cae159f3e967c40708fb7ed3eba5388f91f85c9d
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-10-26 10:09:51 -0700

    Refactor set to prepend underscore for private variable

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage.push(item);
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  this._storage.splice(this._storage.indexOf(item), 1);
 };
 
 /*
